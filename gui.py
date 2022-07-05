6479c033537597df4b7f7bba2f8713e01ca61f16
Liu Jiawei <88jliu88@gmail.com>
2022-07-05 15:08:04 -0400

    feat: add compatibility with raspberry pi

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
 from unigui import UniGui
 from unigui.widget import TextWidget
 from unigui.pygamedisplay import PygameDisplay
+from adafruit_ili9341 import ILI9341
 from unigui.colorscheme import Solarized, VSCode
+import displayio
+import board
 import time
 
 WIDTH = 320
@@ -10,7 +13,14 @@ CS = VSCode.dark
 
 # TODO: Here is where we need to create the object that represents our "display"
 # This is the screen where we want our graphical user interface (GUI) to appear.
-display = PygameDisplay(WIDTH, HEIGHT)
+display_bus = displayio.FourWire(
+    board.SPI(),
+    command=board.D25,
+    chip_select=board.CE0,
+    reset=board.D24,
+    baudrate=24000000
+)
+display = ILI9341(display_bus, WIDTH, HEIGHT)
 
 gui = UniGui(WIDTH, HEIGHT, colorscheme=CS)
 
